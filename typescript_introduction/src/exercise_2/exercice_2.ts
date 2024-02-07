913eecf4a7c1cc61b5ffebdf6608411872f41b98
Hensahi <hency.lara@gmail.com>
2024-02-07 14:38:37 -0400

    solucion parcial ejercicio 2 typescript

---
+++ typescript_introduction/src/exercise_2/exercice_2.ts
@@ -0,0 +1,41 @@
+// # Ejercicio Numero 2
+
+// ## Como administrador de una institucion academica debo crear los usuarios para la institucion.
+// - los usuarios por ahora mas importantes son:
+//     - Admin
+//     - Profesor
+//     - Estudiante
+
+//  - Realizar una interfaz para cada uno de los usuarios.
+//  - las propiedades seran a tu criterio a exepcion de(nomina y active, notas, access).
+//  - Los unicos que pueden tener nomina son el admin y el profesor.
+//  - El unico que puede tener la propiedad notas es el estudiante.
+//  - active debe ser opcional.
+//  - La propiedad access debe heredar de una interfaz llamada NivelDeAcceso
+//  - NivelDeAcceso debe contener la propiedad type.
+//  - El nivel de acceso debe ser dinamico independiente del usuario. 
+
+
+
+interface Persona {
+    name: string;
+    Lastname: string;
+    age: number;
+    isActive?: boolean;
+}
+
+interface Admin extends Persona {
+    department: string;
+    nomina: number;
+}
+
+interface Maestro extends Persona {
+    asignatura: string;
+    nomina: number;
+}
+
+interface Estudiante extends Persona {
+    materia: string;
+    nota: number
+}
+
